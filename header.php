d787cc1aadf0a52283bad33bfc93a0d1aa7b5949
handblack <soporte@miasoftware.net>
2021-03-07 12:54:51 -0500

    iniciando

--- header.php
+++ header.php
@@ -1,1 +1,9 @@
 
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/plugins/highlight/styles/default.css">
+        <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css">
+        <?php wp_head(); ?>
+    </head>
+<body <?php body_class(); ?>>
